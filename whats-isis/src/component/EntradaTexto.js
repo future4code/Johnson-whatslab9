bbf032ef938d920048bd69b5757fbfe8ab4054c4
Isis Pedroni <isispedroni3@gmail.com>
2021-07-25 12:18:35 -0300

    iniciando Projeto

---
+++ whats-isis/src/component/EntradaTexto.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+export class EntradaTexto extends Component {
+    render () {
+        return(
+            <div>
+                <textarea/>
+            <div>
+            <button>Enviar</button>
+            </div>
+            </div>
+        );
+    }
+}
